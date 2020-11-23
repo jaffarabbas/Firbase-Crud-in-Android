13b2e46bf1e9a79768d6d413c5da3e6b58d69624
jaffarabbas <gamaportal8@gmail.com>
2020-11-23 12:29:13 +0500

    11-23-2020

--- Firebase_CRUD/app/src/main/java/com/firebace_crud/MainActivity.java
+++ Firebase_CRUD/app/src/main/java/com/firebace_crud/MainActivity.java
@@ -3,12 +3,28 @@ package com.firebace_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    EditText newrecordenter;
+    Button enterRecord;
+    TextView viewRecord;
+
+    //firebase variables
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference Refer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        Refer=firebaseDatabase.getReference();
     }
 }

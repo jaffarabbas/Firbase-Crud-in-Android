61d7b3655bf51adc4fadc9fae72e0df698293450
jaffarabbas <gamaportal8@gmail.com>
2020-11-28 09:43:37 +0500

    11-28-2020

--- Firebase_CRUD/app/src/main/java/com/firebace_crud/MainActivity.java
+++ Firebase_CRUD/app/src/main/java/com/firebace_crud/MainActivity.java
@@ -3,28 +3,41 @@ package com.firebace_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.core.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText newrecordenter;
     Button enterRecord;
     TextView viewRecord;
-
-    //firebase variables
-    private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference Refer;
-
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference mRef= mDatabase.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initcode();
+        //firebase variables
+        this.enterRecord.setOnClickListener(this::runCode);
+    }
 
-        firebaseDatabase=FirebaseDatabase.getInstance();
-        Refer=firebaseDatabase.getReference();
+    //store data
+    private void runCode(View v){
+        String data = newrecordenter.getText().toString();
+        mRef.setValue(data);
+        Toast.makeText(this,"Data Inserted",Toast.LENGTH_LONG).show();
+    }
+
+    private void initcode(){
+        enterRecord = (Button) findViewById(R.id.enterrcord);
+        newrecordenter = (EditText) findViewById(R.id.entername);
     }
 }
